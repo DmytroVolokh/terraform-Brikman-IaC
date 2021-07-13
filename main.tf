4677e3ccd45204292c5de6375bc5a9e135ee43aa
Dmytro Volokh <Dmytro_Volokh@epam.com>
2021-07-14 00:25:52 +0300

    Created listener ALB

--- main.tf
+++ main.tf
@@ -48,3 +48,17 @@ resource "aws_lb" "example" {
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default.ids
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
