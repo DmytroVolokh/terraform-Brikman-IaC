cf5717120ad1cdf198f8aa91a93beb9b945a204a
Dmytro Volokh <Dmytro_Volokh@epam.com>
2021-07-13 18:38:37 +0300

    Added vyrazhenie vpc_sc_group

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-05f7491af5eef733a"
-  instance_type = "t2.micro"
-  user_data     = <<-EOF
+  ami                    = "ami-05f7491af5eef733a"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
