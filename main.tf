af4705432796b6891d1528ae83c7bb4938049ab3
Dmytro Volokh <Dmytro_Volokh@epam.com>
2021-07-14 00:18:46 +0300

    Changed AMI

--- main.tf
+++ main.tf
@@ -2,15 +2,35 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-05f7491af5eef733a"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  user_data              = file(user_data.sh)
-              EOF
-  tags = {
-    Name = "Brikman-example"
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-05f7491af5eef733a"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = file("user_data.sh")
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 10
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group" "instance" {
