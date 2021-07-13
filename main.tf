4f2865e118e0e2ddfa6c599f184d0c2488151a31
Dmytro Volokh <Dmytro_Volokh@epam.com>
2021-07-13 18:21:03 +0300

    Added bash script in IaC

--- main.tf
+++ main.tf
@@ -5,7 +5,11 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-05f7491af5eef733a"
   instance_type = "t2.micro"
-
+  user_data     = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "Brikman-example"
   }
