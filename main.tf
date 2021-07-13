f8d7b516ae9b0713312a7fe5c421caa23d1282e4
Dmytro Volokh <Dmytro_Volokh@epam.com>
2021-07-14 00:28:33 +0300

    Created SG for ALB

--- main.tf
+++ main.tf
@@ -62,3 +62,19 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
